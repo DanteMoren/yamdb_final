28223681ec08216d8f64ff3df41cc20fd6a83ad0
DanteMoren <73705466+DanteMoren@users.noreply.github.com>
2021-12-06 18:22:20 +0300

    update settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -103,6 +103,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
